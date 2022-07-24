2f3c9b0508502f9e2534ffe5512e3ca59444b80d
Luiz Medeiros <luizpaulo13@gmail.com>
2022-07-24 10:36:05 -0300

    added model.py

---
+++ source/api/model.py
@@ -0,0 +1,31 @@
+"""
+Creator: Luiz Paulo de Souza Medeiros
+Date: Jul 24, 2022
+Define functions used in the model for phishing-detection-2
+"""
+
+import tensorflow as tf
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('target')
+    df = {key: value[:,tf.newaxis] for key, value in dataframe.items()}
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(batch_size)
+    return ds
+
+def get_normalization_layer(name, dataset):
+    # Create a Normalization layer for the feature.
+    normalizer = tf.keras.layers.Normalization(axis=None)
+
+    # Prepare a Dataset that only yields the feature.
+    feature_ds = dataset.map(lambda x, y: x[name])
+
+    # Learn the statistics of the data.
+    normalizer.adapt(feature_ds)
+
+    return normalizer
